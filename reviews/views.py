067d9034f646e8bc8dcd14295382429c70008339
Veniamin Polienko <kasaklalita@mail.ru>
2022-02-06 09:42:21 +0300

    added a CreateView

--- reviews/views.py
+++ reviews/views.py
@@ -5,18 +5,26 @@ from .models import Review
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 # Create your views here.
 
 # View as a class
-class ReviewView(FormView):
+
+class ReviewView(CreateView):
+    model = Review
+    # fields = '__all__'
     form_class = ReviewForm
     template_name = 'reviews/review.html'
     success_url = '/thank-you'
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = 'reviews/review.html'
+#     success_url = '/thank-you'
+#
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
 
     # def get(self, request):
     #     form = ReviewForm()
